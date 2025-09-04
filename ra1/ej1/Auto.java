12c202c6af4f84478233db5c159aa2bf9147c416
profelucas <lucas.almonacid@ulagos.cl>
2025-09-04 12:59:25 -0400

    aplicacion de herencia

--- ra1/ej1/Auto.java
+++ ra1/ej1/Auto.java
@@ -4,13 +4,12 @@ class Auto {
     private String color;
     private String patente;
     private String modelo;
-    private String Combustible;
     private int Annio;
     private float km;
     private int traccion;
 
     //metodo constructor
-    public Auto(String tmarca, String color, String patente, String Modelo, String combustible, int an, int trac, float km){
+    public Auto(String tmarca, String color, String patente, String Modelo, int an, int trac, float km){
         //forma 1
         marca = tmarca;
         //forma 2
@@ -18,7 +17,6 @@ class Auto {
         this.Annio= an;
         this.modelo= Modelo;
         this.patente=patente;
-        this.Combustible=combustible;
         this.traccion=trac;
         this.km=km;
     }
@@ -31,4 +29,54 @@ class Auto {
         this.modelo=modelo;
     }
 
+    public String getmarca(){
+        return this.marca;
+    }
+
+    //funcion compare dos marcas de objetos distintos
+
+    public void compararmarcas(String marca_objeto){
+        if (this.marca==marca_objeto) {
+            System.out.println("Son marcas iguales");
+        } else {
+            System.out.println("Son marcas distintas");
+        }
+    }
+
+    public void compararmarcas2(Auto c1){
+
+        String marca = c1.getmarca();
+
+        if (this.marca == marca) {
+            System.out.println("Son marcas iguales");
+        } else {
+            System.out.println("Son marcas distintas");
+        }
+
+    }
+}
+
+class autoelectrico extends Auto{
+
+    private int capacidadcarga;
+
+    public autoelectrico(String tmarca, String color, String patente, String Modelo, int an, int trac, float km, int capacidadcarga){
+        super(tmarca,color,patente,Modelo, an, trac, km);
+        this.capacidadcarga=capacidadcarga;
+    }
+
+    public int getcarga(){
+        return this.capacidadcarga;
+    }
+
+    // funcion ->> Estado de la bateria()
+    //>>50kw -> cambie la bateria
+
+    public void EstadoBateria(){
+        if (this.capacidadcarga<=50000) {
+            System.out.println("Cambie la bateria");
+        } else {
+            System.out.println("El estado de la bateria es optimo");
+        }
+    }
 }
--- ra1/ej1/Main.java
+++ ra1/ej1/Main.java
@@ -1,13 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         //creacion de objeto 1
-        Auto tesla = new Auto("tesla","blanco","abcd13","S","Electrico",2024,0,0);
-        System.out.println(tesla.getmodelo());
-        Auto toyota = new Auto("toyota", "rojo", "abcd123", "hilux", "diesel", 2006, 1,15000);
+        Auto toyota = new Auto("toyota", "rojo", "abcd123", "hilux", 2006, 1,15000);
+        Auto peugeot = new Auto("peugeot", "amarillo", "abcd124", "206", 2007, 1,1500000);
+        System.out.println("mostrar atributos");
+
         System.out.println(toyota.getmodelo());
-        System.out.printf("el modelo es %s", toyota.getmodelo());
-        //modificacion de objetos
-        tesla.setmodelo("s");
+        System.out.printf("el modelo es %s\n", toyota.getmodelo());
+
+        System.out.println("funciones con objetos de parametros");
+        toyota.compararmarcas2(peugeot);
+
+      
+        autoelectrico tesla = new autoelectrico("tesla", "blanco", "123123", "x", 2024, 1, 0, 100000);
+
+        System.out.println(tesla.getcarga());
         System.out.println(tesla.getmodelo());
+
+        tesla.EstadoBateria();
+        toyota.compararmarcas(tesla.getmarca());
+       
     }
 }
